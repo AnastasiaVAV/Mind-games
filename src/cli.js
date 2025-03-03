13f89a65d7a0a6e5c0259324a4ccc1b7a465523e
nastya volkova <derric41@mail.ru>
2025-03-03 14:13:52 +0400

    4.2 Add Eslint

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const greetUser = () => {
   const name = readlineSync.question('May I have your name? ');
-  let greeting = (`Hello, ${name}!`);
+  const greeting = (`Hello, ${name}!`);
   return greeting;
 };
 
