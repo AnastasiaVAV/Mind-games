e8dd60f4ce804ac83cb92950a0b4a9e524e106ae
nastya volkova <derric41@mail.ru>
2025-03-05 10:10:46 +0400

    fix calc-games.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -4,9 +4,7 @@ import launchTheGame from '../index.js';
 const rules = 'What is the result of the expression?';
 
 const checkExpression = () => {
-  const [calc, result] = getRandomCalc();
-  const question = calc;
-  const correctAnswer = result;
+  const [question, correctAnswer] = getRandomCalc();
   return [question, correctAnswer];
 };
 
