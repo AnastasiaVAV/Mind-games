7f40f819f1b25be0abce2c9df6da7938e88ec8f8
nastya volkova <derric41@mail.ru>
2025-08-26 12:18:29 +0400

    edit eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: pluginJs.configs.recommended,
-});
+})
 
 export default [
   { languageOptions: { globals: globals.node } },
@@ -23,4 +23,4 @@ export default [
       'semi': ['error', 'never'],
     },
   },
-];
+]
